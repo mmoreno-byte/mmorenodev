8c62937ad07c53f02fdc2c611b25bcc1e0e28530
mmoreno-byte <mdmorenoinfor@gmail.com>
2026-07-16 19:12:29 +0200

    cv fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ export default function Hero() {
         </div>
 
         {/* Botón Descargar CV */}
-        <a href="/mmorenodev/mdmoreno.pdf" download className="hero-cv-btn">
+        <a href="/mdmoreno.pdf" download className="hero-cv-btn">
           <FaDownload /> Descargar CV
         </a>
 
